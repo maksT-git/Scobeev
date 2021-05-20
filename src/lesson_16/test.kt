1beddd7dd046d735b60a32525c21210994522555
maksT-git <makstrusha@inbox.ru>
2021-05-20 15:58:33 +0300

    132

--- src/lesson_16/test.kt
+++ src/lesson_16/test.kt
@@ -1,10 +1,11 @@
 package lesson_16
 
 class test {
+
 }
 
 fun main(args: Array<String>) {
-    var p = "С образцом задания на испытательный срок можно ознакомиться ниже: Задачи для проверки квалификации. ... Приводим самый простой пример:"
+    var p = "С задания на испытательный срок можно ознакомиться ниже: Задачи для проверки квалификации. ... Приводим самый простой пример:"
 var s = p.toUpperCase()
    var c1:Char
    var c2:Char
@@ -16,12 +17,12 @@ var s = p.toUpperCase()
 
       if(c1!='.' && c1!=' ' && c1!= ':'&& c1== c2){
           println()
-          print(c1)
-          continue
+
+          //continue
         }
-print(c1)
+        print(c1)
     }
-println()
+
     }
 
 
